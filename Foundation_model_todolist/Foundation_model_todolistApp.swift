bce3a8190aa762d68c80bfcb43f680b9fe5cea46
bimo.ez <bimo.ez@kakaomobility.com>
2025-09-06 22:13:52 +0900

    Initial Commit

---
+++ Foundation_model_todolist/Foundation_model_todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  Foundation_model_todolistApp.swift
+//  Foundation_model_todolist
+//
+//  Created by bimo.ez on 9/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Foundation_model_todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
